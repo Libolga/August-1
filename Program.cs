72f465e24af3680efd8d4c0b2ac6b150495ebe31
Olga <sibgutyb@yandex.ru>
2022-08-16 00:46:18 +1000

    Add 1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+ 
+
+namespace ZAD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] i  = null!;
+             i = Console.ReadLine().Split(' ');
+            
+            int b = Convert.ToInt32(i[1]);
+            
+            Console.WriteLine(b);
+ 
+ 
+ 
+        }
+    }
+}
+
+namespace ZAD3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] i  = null!;
+            string txt = 'Нет 3й цифры';
+            
+             i = Console.ReadLine().Split(' ');
+            
+            int b = Convert.ToInt32(i[2]);
+            if (b == null) {
+                    Console.WriteLine(txt);
+            }
+            Console.WriteLine(b);
+ 
+ 
+ 
+        }
+    }
+}
